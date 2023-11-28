f7cbc0552cb72864d64d6bf4dd3714272f5473d2
CarloCracco13 <134367132+CarloCracco13@users.noreply.github.com>
2023-11-28 16:27:17 +0100

    Update concept.py
    I add the Hangman game!

--- projet_info/concept.py
+++ projet_info/concept.py
@@ -25,5 +25,66 @@ while general_input != "stop" :
             print(str(correct) + " la bonne sollution est : "+  sol)
 
 
+#Hangman game 
+
+def possible_words():
+    words = ["string", "tuple", "boolean", "float", "bytes", "while", "variable", "loop"]
+    return rd.choice(words)
+
+def game():
+    print("==========================================================")
+    print("Welcome to the HANGMAN GAME python-themed!")
+    print("==========================================================")
+   
+
+    chosen_word = possible_words()
+    letters_word = len(chosen_word)
+    guess_word = ["_"] * letters_word
+    wrong_letters = []
+    lives = 5
+    
+    while "_" in guess_word and lives > 0:
+        print("\nWord To Guess:", " ".join(guess_word))
+        print("\nWrong Letters:", " ".join(wrong_letters))
+        print(f"\nRemaining lives: {'❤️ ' * lives}\n")
+
+        choice = input("Choose 'l' to guess a letter or 'w' to guess the word: ").lower()
+
+        if choice == 'l':
+            usr_letter = input("\nTry a letter: ").lower()
+            if usr_letter in wrong_letters or usr_letter in guess_word:
+                print("Letter already tested, try another one!")
+            elif usr_letter in chosen_word:
+                for i in range(letters_word):
+                    if chosen_word[i] == usr_letter:
+                        guess_word[i] = usr_letter
+                if "_" not in guess_word:
+                    print("\nCongratulations, you guessed the word!!! You won the game!!!\U0001F600")
+            else:
+                wrong_letters.append(usr_letter)
+                lives -= 1
+                if lives == 0:
+                    print(f"\nYou have no more lives... you lost \U0001F612. The word was: {chosen_word}")
+
+        
+        elif choice == 'w':
+            usr_word = input("\nTry to guess the word: ").lower()
+            if usr_word == chosen_word:
+                print("\nCongratulations, you guessed the word!!! You won the game!!!\U0001F600")
+                break
+            else:
+                if lives > 0:
+                    lives -= 1
+                    print("\nThe word is not the correct one, you lost one life... ")
+                else:
+                    print(f"\nYou have no more lives... you lost \U0001F612. The word was: {chosen_word}")
+        print("==========================================================")
+       
+
+if general_input == "Hangman game":
+    game()
+
+
+
 
 
