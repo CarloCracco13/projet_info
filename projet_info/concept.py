f421764668f3db4c53f8209582ffee5e6370f751
AlexisRosenfeld <89393969+AlexisRosenfeld@users.noreply.github.com>
2023-11-28 14:09:10 +0100

    Add files via upload

--- projet_info/concept.py
+++ projet_info/concept.py
@@ -2,24 +2,27 @@ import random as rd
 
 questions = { "structure_donnee" : {"dict" : "{:,}" , "list" : "[,]" , "tuple" : "{}" }  , "structure_controle" : { "if":"if" , "while":"while" , "for":"for" , "def":"def" } }
 
-usr_inp = "" 
+general_input = ""
+while general_input != "stop" : 
+    general_input = input("choisissez un jeux parmis ... : " )
+    if general_input == "Python" :
+        usr_inp = "" 
 
-points = 0 
-correct = False
-while usr_inp != "stop" : 
-    correct = False
-    q_type = rd.sample(sorted(questions), 1 ) 
-    q = rd.sample(sorted(questions[q_type[0]]), 1 ) 
+        points = 0 
+        correct = False
+        while usr_inp != "stop" : 
+            correct = False
+            q_type = rd.sample(sorted(questions), 1 ) 
+            q = rd.sample(sorted(questions[q_type[0]]), 1 ) 
+            sol  = questions[q_type[0]][q[0]]
 
-    sol  = questions[q_type[0]][q[0]]
+            usr_inp = input("nombre de points :" + str(points) + "\n"   + q[0] + " : "  ) 
 
-    usr_inp = input("nombre de points :" + str(points) + "\n"   + q[0] + " : "  ) 
+            if usr_inp == sol : 
+             correct = True
+             points += 1 
 
-    if usr_inp == sol : 
-        correct = True
-        points += 1 
-
-    print(str(correct) + " la bonne sollution est : "+  sol)
+            print(str(correct) + " la bonne sollution est : "+  sol)
 
 
 
