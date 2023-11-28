518c5507fb7bddd3a8ee372b3d015450d552b662
AlexisRosenfeld <89393969+AlexisRosenfeld@users.noreply.github.com>
2023-11-28 13:13:38 +0100

    Add files via upload

---
+++ projet_info/concept.py
@@ -0,0 +1,26 @@
+import random as rd 
+
+questions = { "structure_donnee" : {"dict" : "{:,}" , "list" : "[,]" , "tuple" : "{}" }  , "structure_controle" : { "if":"if" , "while":"while" , "for":"for" , "def":"def" } }
+
+usr_inp = "" 
+
+points = 0 
+correct = False
+while usr_inp != "stop" : 
+    correct = False
+    q_type = rd.sample(sorted(questions), 1 ) 
+    q = rd.sample(sorted(questions[q_type[0]]), 1 ) 
+
+    sol  = questions[q_type[0]][q[0]]
+
+    usr_inp = input("nombre de points :" + str(points) + "\n"   + q[0] + " : "  ) 
+
+    if usr_inp == sol : 
+        correct = True
+        points += 1 
+
+    print(str(correct) + " la bonne sollution est : "+  sol)
+
+
+
+
